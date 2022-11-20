77e7ab1e4da0453386251c43776741b93be949eb
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2022-11-20 15:44:00 +0000

    ooo

---
+++ Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace exPoint
+{
+    public class Program
+    {
+        public static Main()
+        {
+
+        }
+    }
+    public class Point
+    {
+        private public int x;   
+        private public int y;   
+    }
+}
